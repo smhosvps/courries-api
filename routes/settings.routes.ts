4e0f2a38a3c96e68409f4ead163f58b3962e7eaa
Perezide <favourkonboye@gmail.com>
2026-04-24 15:24:23 +0100

    dhhd

--- routes/settings.routes.ts
+++ routes/settings.routes.ts
@@ -4,7 +4,7 @@ import { authenticate } from '../middleware/auth';
 
 const keyRouter = Router();
 
-keyRouter.get('/key-settings', authenticate, getSettings);
+keyRouter.get('/key-settings', getSettings);
 keyRouter.put('/update-keys', authenticate, updateSettings);
 
 export default keyRouter;
